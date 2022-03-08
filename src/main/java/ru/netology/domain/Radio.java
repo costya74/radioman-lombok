5f7ce0f2b02934cfb17a0d100c295634d424c8e8
Бояршинов Константин <costya74@bk.ru>
2022-03-08 16:57:54 +0500

    commit new radio

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,16 +3,57 @@ package ru.netology.domain;
 public class Radio {
     private int increaseVolume;
     private int currentStation;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+    private int minStation = 0;
+    private int maxStation = 9;
+    private boolean on;
+
+    public Radio() {   }
+
+    public Radio(int increaseVolume, int minVolume, int maxVolume, boolean on) {
+        this.increaseVolume = increaseVolume;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+        this.on = on;
+    }
+
+    public Radio(int currentStation, int minStation, int maxStation) {
+        this.currentStation = currentStation;
+        this.minStation = minStation;
+        this.maxStation = maxStation;
+    }
+
+    public Radio(int increaseVolume, int currentStation) {
+        this.increaseVolume = increaseVolume;
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
 
     public int getIncreaseVolume() {
         return increaseVolume;
     }
 
     public void setIncreaseVolume(int increaseVolume) {
-        if (increaseVolume > 10) {
+        if (increaseVolume > maxVolume) {
             return;
         }
-        if (increaseVolume < 0) {
+        if (increaseVolume < minVolume) {
             return;
         }
         this.increaseVolume = increaseVolume;
@@ -23,37 +64,46 @@ public class Radio {
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation > 9) {
-            currentStation = 0;
+        if (currentStation > maxStation) {
+            currentStation = minStation;
         }
-        if (currentStation < 0) {
-            currentStation = 9;
+        if (currentStation < minStation) {
+            currentStation = maxStation;
         }
         this.currentStation = currentStation;
     }
+
     public void currentStationByOneForward() {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             setCurrentStation(currentStation + 1);
         }
     }
 
     public void currentStationByOneBack() {
-        if (currentStation > 0) {
+        if (currentStation > minStation) {
             setCurrentStation(currentStation - 1);
         }
     }
 
     public void increaseVolumeByOneForward() {
-        if (increaseVolume < 10) {
+        if (increaseVolume < maxVolume) {
             setIncreaseVolume(increaseVolume + 1);
         }
     }
 
     public void increaseVolumeByOneBack() {
-        if (increaseVolume > 0) {
+        if (increaseVolume > minVolume) {
             setIncreaseVolume(increaseVolume - 1);
         }
     }
 
 }
+
+
+
+
+
+
+
+
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    Radio volume = new Radio();
+    Radio station = new Radio();
+
+    @Test
+    public void sholdRadio() {
+        Radio volume = new Radio();
+
+        Radio volume2 = new Radio(15, 0, 100, true);
+
+        Radio volume3 = new Radio(0, 0, 9);
+
+    }
 
     @Test
     public void sholdincreaseVolumeOutOfRangeMax() {
-        Radio volume = new Radio();
-        volume.setIncreaseVolume(11);
+
+        volume.setIncreaseVolume(101);
         int expected = 0;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
@@ -17,16 +29,16 @@ class RadioTest {
 
     @Test
     public void sholdincreaseVolumeMax() {
-        Radio volume = new Radio();
-        volume.setIncreaseVolume(10);
-        int expected = 10;
+
+        volume.setIncreaseVolume(100);
+        int expected = 100;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
 
     @Test
     public void sholdincreaseVolumeMin() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(0);
         int expected = 0;
         int actual = volume.getIncreaseVolume();
@@ -35,7 +47,7 @@ class RadioTest {
 
     @Test
     public void sholdincreaseVolumeOutOfRangeMin() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(-1);
         int expected = 0;
         int actual = volume.getIncreaseVolume();
@@ -44,7 +56,7 @@ class RadioTest {
 
     @Test
     public void sholdincreaseVolumeInOfRange() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(5);
         int expected = 5;
         int actual = volume.getIncreaseVolume();
@@ -53,7 +65,7 @@ class RadioTest {
 
     @Test
     public void sholdCurrentStation() {
-        Radio station = new Radio();
+
         station.setCurrentStation(5);
         int expected = 5;
         int actual = station.getCurrentStation();
@@ -62,7 +74,7 @@ class RadioTest {
 
     @Test
     public void sholdCurrentStationOutOfRangeMax() {
-        Radio station = new Radio();
+
         station.setCurrentStation(10);
         int expected = 0;
         int actual = station.getCurrentStation();
@@ -71,7 +83,7 @@ class RadioTest {
 
     @Test
     public void sholdCurrentStationOutOfRangeMin() {
-        Radio station = new Radio();
+
         station.setCurrentStation(-1);
         int expected = 9;
         int actual = station.getCurrentStation();
@@ -80,43 +92,47 @@ class RadioTest {
 
     @Test
     public void sholdIncreaseVolumeByOneForward() {
-        Radio volume = new Radio();
-        volume.setIncreaseVolume(10);
+
+        volume.setIncreaseVolume(100);
         volume.increaseVolumeByOneForward();
-        int expected = 10;
+        int expected = 100;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneForwardBonder() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(0);
         volume.increaseVolumeByOneForward();
         int expected = 1;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneForwardBonderMin() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(-1);
         volume.increaseVolumeByOneForward();
         int expected = 1;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneForwardBonderMax() {
-        Radio volume = new Radio();
-        volume.setIncreaseVolume(11);
+
+        volume.setIncreaseVolume(100);
         volume.increaseVolumeByOneForward();
-        int expected = 1;
+        int expected = 100;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneForwardAvg() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(5);
         volume.increaseVolumeByOneForward();
         int expected = 6;
@@ -126,43 +142,47 @@ class RadioTest {
 
     @Test
     public void sholdIncreaseVolumeByOneBack() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(1);
         volume.increaseVolumeByOneBack();
         int expected = 0;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneBackBonderMax() {
-        Radio volume = new Radio();
-        volume.setIncreaseVolume(10);
+
+        volume.setIncreaseVolume(100);
         volume.increaseVolumeByOneBack();
-        int expected = 9;
+        int expected = 99;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdIncreaseVolumeByOneBackBonderMin() {
-        Radio volume = new Radio();
+
         volume.setIncreaseVolume(0);
         volume.increaseVolumeByOneBack();
         int expected = 0;
         int actual = volume.getIncreaseVolume();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdCurrentStationByOneForward() {
-        Radio station = new Radio();
+
         station.setCurrentStation(7);
         station.currentStationByOneForward();
         int expected = 8;
         int actual = station.getCurrentStation();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdCurrentStationByOneForwardBonderMin() {
-        Radio station = new Radio();
+
         station.setCurrentStation(0);
         station.currentStationByOneForward();
         int expected = 1;
@@ -172,16 +192,17 @@ class RadioTest {
 
     @Test
     public void sholdCurrentStationByOneForwardBonderMax() {
-        Radio station = new Radio();
+
         station.setCurrentStation(9);
         station.currentStationByOneForward();
         int expected = 9;
         int actual = station.getCurrentStation();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdCurrentStationByOneBack() {
-        Radio station = new Radio();
+
         station.setCurrentStation(7);
         station.currentStationByOneBack();
         int expected = 6;
@@ -191,16 +212,17 @@ class RadioTest {
 
     @Test
     public void sholdCurrentStationByOneBackBonderMax() {
-        Radio station = new Radio();
+
         station.setCurrentStation(9);
         station.currentStationByOneBack();
         int expected = 8;
         int actual = station.getCurrentStation();
         assertEquals(expected, actual);
     }
+
     @Test
     public void sholdCurrentStationByOneBackBonderMin() {
-        Radio station = new Radio();
+
         station.setCurrentStation(0);
         station.currentStationByOneBack();
         int expected = 0;
@@ -209,4 +231,3 @@ class RadioTest {
     }
 
 }
-
