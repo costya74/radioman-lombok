8527583bc473ec40ee1ae7f1c22217769ed300dc
Бояршинов Константин <costya74@bk.ru>
2022-03-09 14:09:08 +0500

    commit data class

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology.domain;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int increaseVolume;
     private int currentStation;
@@ -8,100 +16,7 @@ public class Radio {
     private int minStation = 0;
     private int maxStation = 9;
     private boolean on;
-
-    public Radio() {   }
-
-    public Radio(int increaseVolume, int minVolume, int maxVolume, boolean on) {
-        this.increaseVolume = increaseVolume;
-        this.minVolume = minVolume;
-        this.maxVolume = maxVolume;
-        this.on = on;
-    }
-
-    public Radio(int currentStation, int minStation, int maxStation) {
-        this.currentStation = currentStation;
-        this.minStation = minStation;
-        this.maxStation = maxStation;
-    }
-
-    public Radio(int increaseVolume, int currentStation) {
-        this.increaseVolume = increaseVolume;
-        this.currentStation = currentStation;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public int getIncreaseVolume() {
-        return increaseVolume;
-    }
-
-    public void setIncreaseVolume(int increaseVolume) {
-        if (increaseVolume > maxVolume) {
-            return;
-        }
-        if (increaseVolume < minVolume) {
-            return;
-        }
-        this.increaseVolume = increaseVolume;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > maxStation) {
-            currentStation = minStation;
-        }
-        if (currentStation < minStation) {
-            currentStation = maxStation;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public void currentStationByOneForward() {
-        if (currentStation < maxStation) {
-            setCurrentStation(currentStation + 1);
-        }
-    }
-
-    public void currentStationByOneBack() {
-        if (currentStation > minStation) {
-            setCurrentStation(currentStation - 1);
-        }
-    }
-
-    public void increaseVolumeByOneForward() {
-        if (increaseVolume < maxVolume) {
-            setIncreaseVolume(increaseVolume + 1);
-        }
-    }
-
-    public void increaseVolumeByOneBack() {
-        if (increaseVolume > minVolume) {
-            setIncreaseVolume(increaseVolume - 1);
-        }
-    }
-
 }
-
-
-
-
 
 
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,233 +1,251 @@
-package ru.netology.domain;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RadioTest {
-    Radio volume = new Radio();
-    Radio station = new Radio();
-
-    @Test
-    public void sholdRadio() {
-        Radio volume = new Radio();
-
-        Radio volume2 = new Radio(15, 0, 100, true);
-
-        Radio volume3 = new Radio(0, 0, 9);
-
-    }
-
-    @Test
-    public void sholdincreaseVolumeOutOfRangeMax() {
-
-        volume.setIncreaseVolume(101);
-        int expected = 0;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdincreaseVolumeMax() {
-
-        volume.setIncreaseVolume(100);
-        int expected = 100;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdincreaseVolumeMin() {
-
-        volume.setIncreaseVolume(0);
-        int expected = 0;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdincreaseVolumeOutOfRangeMin() {
-
-        volume.setIncreaseVolume(-1);
-        int expected = 0;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdincreaseVolumeInOfRange() {
-
-        volume.setIncreaseVolume(5);
-        int expected = 5;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStation() {
-
-        station.setCurrentStation(5);
-        int expected = 5;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationOutOfRangeMax() {
-
-        station.setCurrentStation(10);
-        int expected = 0;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationOutOfRangeMin() {
-
-        station.setCurrentStation(-1);
-        int expected = 9;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneForward() {
-
-        volume.setIncreaseVolume(100);
-        volume.increaseVolumeByOneForward();
-        int expected = 100;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneForwardBonder() {
-
-        volume.setIncreaseVolume(0);
-        volume.increaseVolumeByOneForward();
-        int expected = 1;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneForwardBonderMin() {
-
-        volume.setIncreaseVolume(-1);
-        volume.increaseVolumeByOneForward();
-        int expected = 1;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneForwardBonderMax() {
-
-        volume.setIncreaseVolume(100);
-        volume.increaseVolumeByOneForward();
-        int expected = 100;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneForwardAvg() {
-
-        volume.setIncreaseVolume(5);
-        volume.increaseVolumeByOneForward();
-        int expected = 6;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneBack() {
-
-        volume.setIncreaseVolume(1);
-        volume.increaseVolumeByOneBack();
-        int expected = 0;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneBackBonderMax() {
-
-        volume.setIncreaseVolume(100);
-        volume.increaseVolumeByOneBack();
-        int expected = 99;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdIncreaseVolumeByOneBackBonderMin() {
-
-        volume.setIncreaseVolume(0);
-        volume.increaseVolumeByOneBack();
-        int expected = 0;
-        int actual = volume.getIncreaseVolume();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneForward() {
-
-        station.setCurrentStation(7);
-        station.currentStationByOneForward();
-        int expected = 8;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneForwardBonderMin() {
-
-        station.setCurrentStation(0);
-        station.currentStationByOneForward();
-        int expected = 1;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneForwardBonderMax() {
-
-        station.setCurrentStation(9);
-        station.currentStationByOneForward();
-        int expected = 9;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneBack() {
-
-        station.setCurrentStation(7);
-        station.currentStationByOneBack();
-        int expected = 6;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneBackBonderMax() {
-
-        station.setCurrentStation(9);
-        station.currentStationByOneBack();
-        int expected = 8;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void sholdCurrentStationByOneBackBonderMin() {
-
-        station.setCurrentStation(0);
-        station.currentStationByOneBack();
-        int expected = 0;
-        int actual = station.getCurrentStation();
-        assertEquals(expected, actual);
-    }
-
-}
+//package ru.netology.domain;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class RadioTest {
+//    Radio volume = new Radio();
+//    Radio station = new Radio();
+//
+//    Radio radio = new Radio(25, 0, 0, 100,
+//            0, 9, true);
+//
+//
+//    @Test
+//    public void sholdincreaseVolumeOutOfRangeMax() {
+//
+//        volume.setIncreaseVolume(101);
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//
+//    @Test
+//    public void setVolume(Radio volume) {
+//        this.volume = volume;
+//    }
+//
+//    public void setStation(Radio station) {
+//        this.station = station;
+//    }
+//
+//    public Radio getVolume() {
+//        return volume;
+//    }
+//
+//    public Radio getStation() {
+//        return station;
+//    }
+//
+//    @Test
+//    public void sholdincreaseVolumeOutOfRangeMax() {
+//
+//        volume.setIncreaseVolume(101);
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdincreaseVolumeMax() {
+//
+//        volume.setIncreaseVolume(100);
+//        int expected = 100;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdincreaseVolumeMin() {
+//
+//        volume.setIncreaseVolume(0);
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdincreaseVolumeOutOfRangeMin() {
+//
+//        volume.setIncreaseVolume(-1);
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdincreaseVolumeInOfRange() {
+//
+//        volume.setIncreaseVolume(5);
+//        int expected = 5;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStation() {
+//
+//        station.setCurrentStation(5);
+//        int expected = 5;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationOutOfRangeMax() {
+//
+//        station.setCurrentStation(10);
+//        int expected = 0;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationOutOfRangeMin() {
+//
+//        station.setCurrentStation(-1);
+//        int expected = 9;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneForward() {
+//
+//        volume.setIncreaseVolume(100);
+//
+//        int expected = 100;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneForwardBonder() {
+//
+//        volume.setIncreaseVolume(0);
+//
+//        int expected = 1;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneForwardBonderMin() {
+//
+//        volume.setIncreaseVolume(-1);
+//
+//        int expected = 1;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneForwardBonderMax() {
+//
+//        volume.setIncreaseVolume(100);
+//
+//        int expected = 100;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneForwardAvg() {
+//
+//        volume.setIncreaseVolume(5);
+//
+//        int expected = 6;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneBack() {
+//
+//        volume.setIncreaseVolume(1);
+//
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneBackBonderMax() {
+//
+//        volume.setIncreaseVolume(100);
+//
+//        int expected = 99;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdIncreaseVolumeByOneBackBonderMin() {
+//
+//        volume.setIncreaseVolume(0);
+//
+//        int expected = 0;
+//        int actual = volume.getIncreaseVolume();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneForward() {
+//
+//        station.setCurrentStation(7);
+//
+//        int expected = 8;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneForwardBonderMin() {
+//
+//        station.setCurrentStation(0);
+//
+//        int expected = 1;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneForwardBonderMax() {
+//
+//        station.setCurrentStation(9);
+//
+//        int expected = 9;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneBack() {
+//
+//        station.setCurrentStation(7);
+//
+//        int expected = 6;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneBackBonderMax() {
+//
+//        station.setCurrentStation(9);
+//
+//        int expected = 8;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void sholdCurrentStationByOneBackBonderMin() {
+//
+//        station.setCurrentStation(0);
+//
+//        int expected = 0;
+//        int actual = station.getCurrentStation();
+//        assertEquals(expected, actual);
+//    }
+//}
